77f3b903ba0f4e842e4a3abaf0d0a6420e1561f1
Aguilar Henry <henrysagq@gmail.com>
2020-07-02 15:58:13 -0500

    Http Module, Controller y Service

---
+++ 01-http/introduccion/src/http/http.module.ts
@@ -0,0 +1,11 @@
+import {Module } from "@nestjs/common";
+
+// @Nombre() -> Decorador es como una funcion
+@Module({
+    imports: [],
+    controllers: [],
+    providers: []
+})
+export class HttpModule {
+    
+}
